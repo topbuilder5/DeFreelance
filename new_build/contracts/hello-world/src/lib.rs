543332d029fa1d9d983def05e0729120e929b19d
topbuilder5 <luiscordes25@gmail.com>
2025-10-07 23:56:31 +0900

    add test func feature

--- new_build/contracts/hello-world/src/lib.rs
+++ new_build/contracts/hello-world/src/lib.rs
@@ -103,3 +103,21 @@ impl BountyContract {
 }
 
 mod test;
+
+//add test feature
+
+fn test() {
+    let env = Env::default();
+    let contract_id = env.register(Contract, ());
+    let client = ContractClient::new(&env, &contract_id);
+
+    let words = client.hello(&String::from_str(&env, "Dev"));
+    assert_eq!(
+        words,
+        vec![
+            &env,
+            String::from_str(&env, "Hello"),
+            String::from_str(&env, "Dev"),
+        ]
+    );
+}
