b17ea9a0ea742cb85d147f91936025f1aa7f9d8a
topbuilder5 <luiscordes25@gmail.com>
2025-10-13 23:59:04 +0900

    update test func feature

--- next.config.js
+++ next.config.js
@@ -6,4 +6,24 @@ const nextConfig = {
     styledComponents: true,
   },
 }
-module.exports = nextConfig;                
+module.exports = nextConfig;   
+
+
+// ---update test function---
+
+
+fn test() {
+    let env = Env::default();
+    let contract_id = env.register(Contract, ());
+    let client = ContractClient::new(&env, &contract_id);
+
+    let words = client.hello(&String::from_str(&env, "Dev"));
+    assert_eq!(
+        words,
+        vec![
+            &env,
+            String::from_str(&env, "Hello"),
+            String::from_str(&env, "Dev"),
+        ]
+    );
+}
