410d89ffa200561b9b30faa53a35cbeb12e9bdad
topbuilder5 <luiscordes25@gmail.com>
2025-10-21 01:24:22 +0900

    update Managed files(#18302)

--- next.config.js
+++ next.config.js
@@ -12,18 +12,3 @@ module.exports = nextConfig;
 // ---update test function---
 
 
-fn test() {
-    let env = Env::default();
-    let contract_id = env.register(Contract, ());
-    let client = ContractClient::new(&env, &contract_id);
-
-    let words = client.hello(&String::from_str(&env, "Dev"));
-    assert_eq!(
-        words,
-        vec![
-            &env,
-            String::from_str(&env, "Hello"),
-            String::from_str(&env, "Dev"),
-        ]
-    );
-}
